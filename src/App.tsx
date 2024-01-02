6d3a5e974c1b663d9094cbaadfe16b7e3229a867
Dileepa Sam <50067526+dnsamw@users.noreply.github.com>
2024-01-02 13:32:10 +0530

    7. Connecting to the Backend (69m) - re usable API client service

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import axios, { CanceledError } from "axios";
 import { useEffect, useState } from "react";
+import apiClient, { CanceledError } from "./services/api-client";
 
 interface User {
   id: number;
@@ -14,8 +14,8 @@ function App() {
   useEffect(() => {
     const controller = new AbortController();
     setLoading(true);
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+    apiClient
+      .get<User[]>("/users", {
         signal: controller.signal,
       })
       .then((res) => {
@@ -35,12 +35,10 @@ function App() {
     const originalUsers = [...users];
     const newUsers = users.filter((user) => user.id !== u.id);
     setUsers(newUsers);
-    axios
-      .delete("https://jsonplaceholder.typicode.com/users/" + u.id)
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
+    apiClient.delete("/users/" + u.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   };
 
   const addUser = () => {
@@ -51,8 +49,8 @@ function App() {
     };
 
     setUsers([newUser, ...users]);
-    axios
-      .post("https://jsonplaceholder.typicode.com/users/", newUser)
+    apiClient
+      .post("/users/", newUser)
       .then(({ data: addedUser }) => {
         setError("");
         setUsers([addedUser, ...users]);
@@ -68,11 +66,8 @@ function App() {
     const updatedUser = { ...user, name: user.name + "*" };
     setUsers(users.map((u) => (user.id === u.id ? updatedUser : u)));
 
-    axios
-      .patch(
-        "https://jsonplaceholder.typicode.com/users/" + user.id,
-        updatedUser
-      )
+    apiClient
+      .patch("/users/" + user.id, updatedUser)
       .then(() => {
         setError("");
       })
